0bde1554fc7f59c6b1796aab44ac9c497a00230e
Cameron Spencer <cameron@lab43.com>
2021-02-24 19:03:33 -0800

    improve error handling

--- utils/logger.js
+++ utils/logger.js
@@ -1,3 +1,11 @@
 exports.log = (...messages) => {
   console.log(...messages);
 }
+
+exports.error = (err, message) => {
+  if (message) {
+    console.log(message);
+  }
+  console.log(err);
+  process.exit();
+}
